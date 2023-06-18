3fc89073f1061b6a589b52ac77274e1152b4a4f8
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-18 18:22:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function threeSum(arr, target) {
 // write your code here
+	let closestSum = Infinity;
+  let minDiff = Infinity;
 	
+	for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const sum = nums[i] + nums[j] + nums[k];
+        const diff = Math.abs(sum - target);
 
-  
+        if (diff < minDiff) {
+          minDiff = diff;
+          closestSum = sum;
+        }
+      }
+    }
+  }
+
+  return closestSum;
+}  
 }
 
 module.exports = threeSum;
